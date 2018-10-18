cfe969d96fbe42e1a47091144634cbf285928ce6
maxim218 <maxim218@yandex.ru>
2018-10-18 22:09:51 +0300

    ok

---
+++ index.js
@@ -0,0 +1,229 @@
+"use strict";
+
+/**
+ * убрать из строки все пробелы
+ * @param codeParam
+ * @returns {string}
+ */
+function killSpaces(codeParam) {
+    return codeParam.toString().split(" ").join("");
+}
+
+/**
+ * убрать из строки все переносы строк
+ * @param codeParam
+ * @returns {string}
+ */
+function killNextLineChar(codeParam) {
+    return codeParam.toString().split("\n").join("");
+}
+
+/**
+ * разбить строку на массив операций по разделителю
+ * @param codeParam
+ * @returns {string[]}
+ */
+function getOperationsArray(codeParam) {
+    return codeParam.toString().split(";");
+}
+
+/**
+ * проверка: является ли строка именем переменной
+ * @param strParam
+ * @returns {boolean}
+ */
+function isVariableName(strParam) {
+    if(strParam.length !== 1) {
+        return false;
+    }
+    return "ABCDEFGHIJKLMNOPQRSTUVWXYZ".indexOf(strParam) !== -1;
+}
+
+/**
+ * проверка: является ли строка математической операцией
+ * @param charParam
+ * @returns {boolean}
+ */
+function isMath(charParam) {
+    if(charParam.toString() === "+") return true;
+    if(charParam.toString() === "-") return true;
+    if(charParam.toString() === "*") return true;
+    if(charParam.toString() === "/") return true;
+    return charParam.toString() === "%";
+}
+
+/**
+ * определение типа операции
+ * @param operationString
+ * @returns {string}
+ */
+function getOperationType(operationString) {
+    if(operationString.length === 0) {
+        return "EMPTY";
+    }
+
+    if(operationString.length === 1) {
+        if(isVariableName(operationString.charAt(0))) {
+            return "WRITE_VARIABLE";
+        }
+    }
+
+    if(operationString.length === 3) {
+        if(isVariableName(operationString.charAt(0))) {
+            if(isVariableName(operationString.charAt(2))) {
+                if(operationString.charAt(1) === "=") {
+                    return "VARIABLE_SET_VARIABLE";
+                }
+            }
+        }
+    }
+
+    if(operationString.length === 5) {
+        if (isVariableName(operationString.charAt(0))) {
+            if (isVariableName(operationString.charAt(2))) {
+                if (isVariableName(operationString.charAt(4))) {
+                    if (operationString.charAt(1) === "=") {
+                        if (isMath(operationString.charAt(3))) {
+                            return "MATH_OPERATION";
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    if(isVariableName(operationString.charAt(0))) {
+        if(operationString.charAt(1) === "=") {
+            const rightPart = operationString.split("=")[1].toString();
+            if(Number.isInteger(rightPart - 1 + 1)) {
+                return "VARIABLE_SET_NUMBER";
+            }
+        }
+    }
+
+    throw new Error();
+}
+
+/**
+ * задать значение переменной
+ * @param variables
+ * @param key
+ * @param value
+ */
+function setVariable(variables, key, value) {
+    variables[key.toString()] = parseInt(value.toString());
+}
+
+/**
+ * получить значение переменной
+ * @param variables
+ * @param key
+ * @returns {number}
+ */
+function getVariable(variables, key) {
+    const value = variables[key.toString()];
+    if(!value) {
+        return 0;
+    }
+    return parseInt(value);
+}
+
+/**
+ * операция присвоения одной переменной значения другой переменной
+ * @param operationString
+ * @param variables
+ */
+function variableSetVariable(operationString, variables) {
+    const value = getVariable(variables, operationString.charAt(2));
+    setVariable(variables, operationString.charAt(0), value);
+}
+
+/**
+ * операция присвоения переменной числового значения
+ * @param operationString
+ * @param variables
+ */
+function variableSetNumber(operationString, variables) {
+    const value = parseInt(operationString.split("=")[1]);
+    setVariable(variables, operationString.charAt(0), value);
+}
+
+/**
+ * операция выполнения математического действия над переменными
+ * @param operationString
+ * @param variables
+ * @returns {null}
+ */
+function mathOperation(operationString, variables) {
+    const key_1 = operationString.charAt(0);
+    const key_2 = operationString.charAt(2);
+    const key_3 = operationString.charAt(4);
+    const math = operationString.charAt(3);
+    const value_2 = parseInt(getVariable(variables, key_2).toString());
+    const value_3 = parseInt(getVariable(variables, key_3).toString());
+    if(math === "+") {
+        setVariable(variables, key_1, parseInt(value_2 + value_3));
+        return null;
+    }
+    if(math === "-") {
+        setVariable(variables, key_1, parseInt(value_2 - value_3));
+        return null;
+    }
+    if(math === "*") {
+        setVariable(variables, key_1, parseInt(value_2 * value_3));
+        return null;
+    }
+    if(math === "/") {
+        if(value_3 !== 0) {
+            setVariable(variables, key_1, parseInt(value_2 / value_3));
+        } else {
+            throw new Error();
+        }
+        return null;
+    }
+    if(math === "%") {
+        if(value_3 !== 0) {
+            setVariable(variables, key_1, parseInt(value_2 % value_3));
+        } else {
+            throw new Error();
+        }
+        return null;
+    }
+}
+
+/**
+ * последовательное выполнение всех операций
+ * @param operationsArray
+ * @returns {number}
+ */
+function visitAllOperations(operationsArray) {
+    const variables = {};
+    for(let i = 0; i < operationsArray.length; i++) {
+        const operationString = operationsArray[i].toString();
+        const operation = getOperationType(operationString.toString());
+        switch (operation) {
+            case "VARIABLE_SET_VARIABLE": variableSetVariable(operationString, variables); break;
+            case "VARIABLE_SET_NUMBER": variableSetNumber(operationString, variables); break;
+            case "MATH_OPERATION": mathOperation(operationString, variables); break;
+        }
+        if(operation === "WRITE_VARIABLE") {
+            return parseInt(getVariable(variables, operationString.charAt(0)).toString());
+        }
+    }
+    return 0;
+}
+
+/**
+ * главная функция для вызова
+ * @param code
+ * @returns {*}
+ */
+function main(code) {
+    let result = undefined;
+    try {
+        result = visitAllOperations(getOperationsArray(killNextLineChar(killSpaces(code.toString()))));
+        return parseInt(result.toString());
+    } catch (err) {
+        return "ERROR";
+    }
+}
---
+++ test.js
@@ -0,0 +1,101 @@
+"use strict";
+
+describe("killSpaces", function () {
+    it("убрать пробелы из функции", function () {
+        assert.equal(killSpaces("hello my world"), "hellomyworld");
+        assert.equal(killSpaces("aaa   bbb   ccc"), "aaabbbccc");
+        assert.equal(killSpaces("abcd"), "abcd");
+    });
+});
+
+describe("isVariableName", function () {
+    it("является ли строка именем переменной", function () {
+        assert.equal(isVariableName("A"), true);
+        assert.equal(isVariableName("G"), true);
+        assert.equal(isVariableName("Z"), true);
+        assert.equal(isVariableName("a"), false);
+        assert.equal(isVariableName("b"), false);
+        assert.equal(isVariableName("5"), false);
+        assert.equal(isVariableName("45"), false);
+        assert.equal(isVariableName("AA"), false);
+        assert.equal(isVariableName("Aa"), false);
+        assert.equal(isVariableName("aA"), false);
+        assert.equal(isVariableName(" "), false);
+        assert.equal(isVariableName("+"), false);
+    });
+});
+
+describe("getOperationType", function () {
+   it("получение типа операции", function () {
+       assert.equal(getOperationType("A"), "WRITE_VARIABLE");
+       assert.equal(getOperationType("Z"), "WRITE_VARIABLE");
+       assert.equal(getOperationType("A=B"), "VARIABLE_SET_VARIABLE");
+       assert.equal(getOperationType("K=K"), "VARIABLE_SET_VARIABLE");
+       assert.equal(getOperationType("X=5"), "VARIABLE_SET_NUMBER");
+       assert.equal(getOperationType("X=55"), "VARIABLE_SET_NUMBER");
+       assert.equal(getOperationType("X=555"), "VARIABLE_SET_NUMBER");
+       assert.equal(getOperationType("X=-5"), "VARIABLE_SET_NUMBER");
+       assert.equal(getOperationType("X=-55"), "VARIABLE_SET_NUMBER");
+       assert.equal(getOperationType("X=0"), "VARIABLE_SET_NUMBER");
+       assert.equal(getOperationType(""), "EMPTY");
+       assert.equal(getOperationType("A=B+C"), "MATH_OPERATION");
+       assert.equal(getOperationType("A=B-C"), "MATH_OPERATION");
+       assert.equal(getOperationType("A=B*C"), "MATH_OPERATION");
+       assert.equal(getOperationType("A=B/C"), "MATH_OPERATION");
+       assert.equal(getOperationType("A=B%C"), "MATH_OPERATION");
+   });
+});
+
+describe("main", function() {
+    it("вывод на экран существующей переменной", function() {
+        assert.equal(main("A=25;A;"), 25);
+        assert.equal(main("Z=123;Q=34;Z;"), 123);
+    });
+
+    it("вывод на экран незаданной переменной", function() {
+        assert.equal(main("A;"), 0);
+        assert.equal(main("A=12;B;"), 0);
+        assert.equal(main("A=34;B=123;W;"), 0);
+    });
+
+    it("использование базовых арифметических операций", function() {
+        assert.equal(main("A=12;B=18;C=A+B;C;"), 30);
+        assert.equal(main("A=12;B=18;C=A-B;C;"), -6);
+        assert.equal(main("A=12;B=18;C=A*B;C;"), 216);
+        assert.equal(main("A=12;B=18;C=A/B;C;"), 0);
+        assert.equal(main("A=12;B=18;C=A%B;C;"), 12);
+        assert.equal(main("X=26;Y=7;A=X/Y;A;"), 3);
+        assert.equal(main("X=26;Y=7;A=X%Y;A;"), 5);
+    });
+
+    it("попытка деления на ноль", function () {
+        assert.equal(main("N=25;X=0;A=N/X;A;"), "ERROR");
+        assert.equal(main("N=-12;X=0;A=N/X;A;"), "ERROR");
+        assert.equal(main("N=0;X=0;A=N/X;A;"), "ERROR");
+    });
+
+    it("попытка взять остаток от деления на ноль", function() {
+        assert.equal(main("T=12;P=0;F=T%P;F;"), "ERROR");
+        assert.equal(main("T=-123;P=0;F=T%P;F;"), "ERROR");
+        assert.equal(main("T=0;P=0;F=T%P;F;"), "ERROR");
+    });
+
+    it("составные арифметические операции", function () {
+        assert.equal(main("A=12;B=15;S=A+B;D=2;K=S*D;P=K;P;"), 54);
+        assert.equal(main("X=56;Y=-123;Z=34;A=0;A=A+X;A=A+Y;A=A+Z;A;"), -33);
+        assert.equal(main("A=19;B=15;X=A+B;C=10;D=7;Y=C+D;V=X/Y;V;"), 2);
+    });
+
+    it("ввод некорректных данных", function() {
+        assert.equal(main("HELLO"), "ERROR");
+        assert.equal(main("X=25+34;X;"), "ERROR");
+        assert.equal(main("A=5;B=6;C=7;X=A+B+C;X;"), "ERROR");
+        assert.equal(main("7;"), "ERROR");
+        assert.equal(main("4=5;"), "ERROR");
+    });
+
+    it("присвоение значения несуществующей переменной", function () {
+        assert.equal(main("A=B;A;"), 0);
+        assert.equal(main("A=123;A=B;A;"), 0);
+    });
+});
